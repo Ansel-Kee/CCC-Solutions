a8f7dc8311a4899404c03f8daa0e1760b227a6a9
Ansel-Kee <62891187+Ansel-Kee@users.noreply.github.com>
2021-02-22 22:39:14 +0800

    Add files via upload

--- CCC 2021/CCC_2021_q5_v2.py
+++ CCC 2021/CCC_2021_q5_v2.py
@@ -1,20 +1,18 @@
-m, n, k = int(input()), int(input()), int(input())
+import sys
+m, n, k = int(sys.stdin.readline()), int(sys.stdin.readline()), int(sys.stdin.readline())
 moves = {}
 
 for i in range(k):
-    move = tuple(input().split())
+    move = tuple(sys.stdin.readline().split())
     try:
         moves[move] += 1
     except:
         moves[move] = 1
 
-R_count, C_count = 0, 0
 
+ans={'R':0, 'C':0}
 for j in moves:
     if moves[j]%2:
-        if j[0] == 'R':
-            R_count += 1
-        else:
-            C_count += 1
-            
+        ans[j[0]] += 1
+R_count, C_count = ans['R'], ans['C']          
 print(R_count*n + C_count*m - R_count*C_count*2)
